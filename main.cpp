fab451140b1a11e042e65ec73fa07a4bdc49d619
ryuzu001 <ryuzu001@ucr.edu>
2017-02-12 02:17:49 +0000

    added strtok test to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 int main(){
-    string user_input;
+    string userInput;  
+    
     cout << "$ ";
-    cin >> user_input;
+    getline(cin, userInput);
+    
+    /* testing to make sure I understand strtok */
+    char * str = new char[userInput.length() + 1];  // char array named str
+    
+    strcpy(str, userInput.c_str());                 // copy string to char* (NOT CONST)
+    
+    char * pch;  
+    string delim = ";";    // delimiters for strtok
+    
+    cout << "\nHere are your commands: \n";
+  
+    pch = strtok (str,delim.c_str());      // break at every ';'
+    while (pch != NULL){
+        cout << pch << "\n";
+        pch = strtok (NULL, delim.c_str());
+    }
+    
+    delete [] str;  //no memory leaks
+    delete [] pch;
 }

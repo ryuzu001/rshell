d1796b6b7a3bb3ca13689d68ad90bcc9e303bd52
ryuzu001 <ryuzu001@ucr.edu>
2017-02-14 00:55:49 +0000

    added exit_shell

---
+++ exit_shell.cpp
@@ -0,0 +1,15 @@
+#include <unistd.h> //execvp
+
+void exit_shell(){
+    char * args[3];
+    string arg1 = "echo";
+    string arg2 = "exiting...";
+    
+    args[0] = (char*) arg1.c_str();  //prints exiting... then exits
+    args[1] = (char*) arg2.c_str();
+    args[2] = NULL;
+    
+    if( execvp (args [0],args) == -1){  //execute, with error checking
+        perror ("exit");
+    }
+}
--- main.cpp
+++ main.cpp
@@ -9,14 +9,23 @@
 #include <vector>
 
 #include "execute.cpp"
+#include "exit_shell.cpp"
 
 
 using namespace std;
 
 int main(){
+    string exitStr = "exit";     //string to indicate exit shell
     string userInput;  
     vector<char*> toExecute;
     
+    cout << "-----------------------\n";
+    cout << "|  Welcome to rshell  |\n";
+    cout << "|  Type any command   |\n";
+    cout << "| Type 'exit' to exit |\n";
+    cout << "-----------------------\n";
+    
+    //WILL NEED TO PUT THIS IN A LOOP LATER
     cout << "$ ";
     getline(cin, userInput);
     
@@ -39,14 +48,25 @@ int main(){
     string delim = " ";    // delimiters for strtok    
   
     pch = strtok (str,delim.c_str());      // break at every delimiter
+    
     while (pch != NULL){
         // cout << pch << endl;
         toExecute.push_back(pch);
         pch = strtok (NULL, delim.c_str());
     }
     
-    execute(toExecute);
+    // Probably put this in a loop later
+    if(toExecute.size() == 0){
+        printf("error: nothing to execute\n");
+        exit(0);
+    }
     
+    if(toExecute.at(0) == exitStr){
+        exit_shell();
+        exit(0);
+    }
+    
+    execute(toExecute);
     
     delete [] str;  //no memory leaks
     // delete [] pch;
